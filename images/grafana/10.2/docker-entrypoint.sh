dae9ae21af2195fa2e62c662fd5f2122b1557ae5
CI Build <CI@i2group.com>
2023-11-30 14:59:56 +0000

    Update images

---
+++ images/grafana/10.2/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# i2, i2 Group, the i2 Group logo, and i2group.com are trademarks of N.Harris Computer Corporation.
+# © N.Harris Computer Corporation (2022-2023)
+#
+# SPDX short identifier: MIT
+
+set -e
+
+. /opt/environment.sh
+
+if [[ ${SSL_ENABLED} == true ]]; then
+  file_env 'SSL_PRIVATE_KEY'
+  file_env 'SSL_CERTIFICATE'
+  if [[ -z ${SSL_PRIVATE_KEY} || -z ${SSL_CERTIFICATE} ]]; then
+    echo "Missing security environment variables. Please check SSL_PRIVATE_KEY SSL_CERTIFICATE" >&2
+    exit 1
+  fi
+
+  TMP_SECRETS=/tmp/i2acerts
+  KEY=${TMP_SECRETS}/server.key
+  CER=${TMP_SECRETS}/server.cer
+
+  if [[ -d ${TMP_SECRETS} ]]; then
+    rm -r ${TMP_SECRETS}
+  fi
+  mkdir ${TMP_SECRETS}
+
+  echo "${SSL_PRIVATE_KEY}" >"${KEY}"
+  echo "${SSL_CERTIFICATE}" >"${CER}"
+
+  export GF_SERVER_PROTOCOL="https"
+  export GF_SERVER_CERT_FILE="${CER}"
+  export GF_SERVER_CERT_KEY="${KEY}"
+fi
+
+set +e
+
+# Call original grafana entrypoint
+exec /run.sh "$@"
