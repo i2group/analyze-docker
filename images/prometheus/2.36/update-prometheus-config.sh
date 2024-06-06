870d4e1ed1e3a72f26b12d26a1cd33f8ab9d54ee
CI Build <CI@i2group.com>
2024-06-06 16:38:36 +0000

    Update images

--- images/prometheus/2.36/update-prometheus-config.sh
+++ images/prometheus/2.36/update-prometheus-config.sh
@@ -17,7 +17,6 @@ if [[ "${LIBERTY_SSL_CONNECTION}" == "true" ]]; then
 else
   LIBERTY_SCHEME="http"
 fi
-export PROMETHEUS_SCHEME LIBERTY_SCHEME
 
 if [ -f "/tmp/prometheus/prometheus.yml" ]; then
   cp /tmp/prometheus/prometheus.yml /etc/prometheus/prometheus.yml
@@ -25,22 +24,23 @@ else
   cp /etc/prometheus-templates/prometheus.yml /etc/prometheus/prometheus.yml
 fi
 
-NON_ENCRYPTED_PASSWORD="${PROMETHEUS_PASSWORD}"
-
-# Substitute environment variables
-envsubst < /etc/prometheus/prometheus.yml > /etc/prometheus/prometheus.yml.tmp
-
-# Replace original file with the new one
-mv /etc/prometheus/prometheus.yml.tmp /etc/prometheus/prometheus.yml
+# We can't use envsubst to replace variables in the prometheus.yml file because it contains $ signs
+sed -ci \
+  -e "s~\${PROMETHEUS_SCHEME}~${PROMETHEUS_SCHEME}~g" \
+  -e "s~\${LIBERTY_SCHEME}~${LIBERTY_SCHEME}~g" \
+  -e "s~\${LIBERTY_ADMIN_USERNAME}~${LIBERTY_ADMIN_USERNAME}~g" \
+  -e "s~\${LIBERTY_ADMIN_PASSWORD}~${LIBERTY_ADMIN_PASSWORD}~g" \
+  -e "s~\${PROMETHEUS_USERNAME}~${PROMETHEUS_USERNAME}~g" \
+  -e "s~\${PROMETHEUS_PASSWORD}~${PROMETHEUS_PASSWORD}~g" \
+  -e "s~\${LIBERTY1_STANZA}~${LIBERTY1_STANZA}~g" \
+  -e "s~\${LIBERTY2_STANZA}~${LIBERTY2_STANZA}~g" \
+  "/etc/prometheus/prometheus.yml"
 
 # Passwords have to be hashed with bcrypt for web-config.yml
-encoded_pass=$(echo "${NON_ENCRYPTED_PASSWORD}" | htpasswd -niBC 10 "" | tr -d ':\n')
-export PROMETHEUS_PASSWORD="${encoded_pass}"
+encoded_pass=$(echo "${PROMETHEUS_PASSWORD}" | htpasswd -niBC 10 "" | tr -d ':\n')
+escaped_encoded_pass=$(printf '%s\n' "$encoded_pass" | sed -e 's/[\/&]/\\&/g')
 
-# Substitute environment variables
-envsubst < /etc/prometheus/web-config.yml > /etc/prometheus/web-config.yml.tmp
-
-# Replace original file with the new one
-mv /etc/prometheus/web-config.yml.tmp /etc/prometheus/web-config.yml
-
-export PROMETHEUS_PASSWORD="${NON_ENCRYPTED_PASSWORD}"
+sed -ci \
+  -e "s/\${PROMETHEUS_USERNAME}/${PROMETHEUS_USERNAME}/g" \
+  -e "s/\${PROMETHEUS_PASSWORD}/${escaped_encoded_pass}/g" \
+  "/etc/prometheus/web-config.yml"
