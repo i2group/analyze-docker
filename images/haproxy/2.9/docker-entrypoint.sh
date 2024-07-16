bbba8abd965870fbf998d66733710301f7cc9cf0
CI Build <CI@i2group.com>
2024-07-16 18:29:02 +0000

    Update images

--- images/haproxy/2.9/docker-entrypoint.sh
+++ images/haproxy/2.9/docker-entrypoint.sh
@@ -13,27 +13,42 @@ fi
 
 . /opt/environment.sh
 
+TMP_SECRETS=/tmp/i2acerts
+
+# Combined cert and key
+CERTIFICATES_FILE="${TMP_SECRETS}/i2Analyze.pem"
+GATEWAY_CERTIFICATES_FILE="${TMP_SECRETS}/gateway.user.pem"
+
+# CA files
+EXTERNAL_CA_FILE="${TMP_SECRETS}/externalCA.cer"
+INTERNAL_CA_FILE="${TMP_SECRETS}/internalCA.cer"
+
+if [[ -d "${TMP_SECRETS}" ]]; then
+  rm -r "${TMP_SECRETS}"
+fi
+mkdir -p "${TMP_SECRETS}"
+
 if [[ "${SERVER_SSL}" == "true" ]]; then
   file_env 'SSL_CERTIFICATE'
   file_env 'SSL_PRIVATE_KEY'
-  if [[ -z "${SSL_PRIVATE_KEY}" || -z "${SSL_CERTIFICATE}" ]]; then
-    echo "Missing security environment variables. Please check SSL_PRIVATE_KEY SSL_CERTIFICATE" >&2
+  if [[ -z "${SSL_PRIVATE_KEY}" || -z "${SSL_CERTIFICATE}" || -z "${SSL_CA_CERTIFICATE}" ]]; then
+    echo "Missing security environment variables. Please check SSL_PRIVATE_KEY SSL_CERTIFICATE SSL_CA_CERTIFICATE" >&2
     exit 1
   fi
 
-  TMP_SECRETS=/tmp/i2acerts
-  CERTIFICATES_FILE="${TMP_SECRETS}/i2Analyze.pem"
-
-  if [[ -d "${TMP_SECRETS}" ]]; then
-    rm -r "${TMP_SECRETS}"
-  fi
-  mkdir -p "${TMP_SECRETS}"
-
-  if [[ -f "${CERTIFICATES_FILE}" ]]; then
-    rm "${CERTIFICATES_FILE}"
-  fi
   echo "${SSL_CERTIFICATE}" >>"${CERTIFICATES_FILE}"
   echo "${SSL_PRIVATE_KEY}" >>"${CERTIFICATES_FILE}"
+  echo "${SSL_CA_CERTIFICATE}" >>"${EXTERNAL_CA_FILE}"
+fi
+
+if [[ "${GATEWAY_SSL_CONNECTION}" == "true" ]]; then
+  if [[ -z "${SSL_OUTBOUND_PRIVATE_KEY}" || -z "${SSL_OUTBOUND_CERTIFICATE}" || -z "${SSL_OUTBOUND_CA_CERTIFICATE}" ]]; then
+    echo "Missing security environment variables. Please check SSL_OUTBOUND_PRIVATE_KEY SSL_OUTBOUND_CERTIFICATE SSL_OUTBOUND_CA_CERTIFICATE" >&2
+    exit 1
+  fi
+  echo "${SSL_OUTBOUND_PRIVATE_KEY}" >>"${GATEWAY_CERTIFICATES_FILE}"
+  echo "${SSL_OUTBOUND_CERTIFICATE}" >>"${GATEWAY_CERTIFICATES_FILE}"
+  echo "${SSL_OUTBOUND_CA_CERTIFICATE}" >>"${INTERNAL_CA_FILE}"
 fi
 
 sudo chown -R haproxy /usr/local/etc/haproxy
